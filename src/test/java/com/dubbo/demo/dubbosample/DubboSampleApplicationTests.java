5e415e177f28fe74843d23cc102c50a7c52bf6af
imeng <you@example.com>
2021-08-05 23:07:59 +0800

    init

---
+++ src/test/java/com/dubbo/demo/dubbosample/DubboSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dubbo.demo.dubbosample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DubboSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
